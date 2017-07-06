08c0f317b041b4c05faca78cdb1f2b0dcc92c576
vmaddur <vamaddur@gmail.com>
2017-07-06 15:53:48 +0530

    USACO 2011 Gold November Contest (http://www.usaco.org/index.php?page=viewproblem2&cpid=91)

---
+++ USACO/2011 Gold November Contest/median.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+
+using namespace std;
+
+struct BIT{
+    int maxi;
+    long long tree [200002];
+    BIT(int m): maxi(m) {}
+    void add(int pos, long long x){
+        while(pos <= maxi){
+            tree[pos] += x;
+            pos += (pos&-pos);
+        }
+    }
+    long long query(int pos){
+        long long sum = 0ll;
+        while(pos > 0){
+            sum += tree[pos];
+            pos -= (pos&-pos);
+        }
+        return sum;
+    }
+};
+
+int numCows, goal, curr = 100001;
+long long ret = 0;
+
+int main(){
+    freopen("median.in", "r", stdin); freopen("median.out", "w", stdout);
+    scanf("%d %d", &numCows, &goal);
+    BIT fenwick(200001); fenwick.add(curr, 1);
+    for(int i = 1; i <= numCows; i++){
+        int x; scanf("%d", &x);
+        if(x < goal) curr--;
+        else curr++;
+        ret += fenwick.query(curr); fenwick.add(curr, 1);
+    }
+    cout << ret << endl;
+    return 0;
+}
