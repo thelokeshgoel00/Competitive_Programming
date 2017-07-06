6ad2577caa8dbf2cdb73c8ef0997bc4c461f306f
vmaddur <vamaddur@gmail.com>
2017-07-06 15:19:07 +0530

    Create tower.cpp
    Problem Source: CEOI 2010 (http://ceoi2010.ics.upjs.sk/pub/documents/ceoi_summary.pdf)

---
+++ Miscellaneous/tower.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+
+int main(){
+  int numCubes, tolerance;
+  long long mod = 1000000009ll, res = 1ll;
+  scanf("%d%d", &numCubes, &tolerance); int cubes[numCubes];
+  for(int i = 0; i < numCubes; i++){
+    int x; scanf("%d", &x); cubes[i] = x;
+  }
+  std::sort(cubes, cubes+numCubes);
+  int p1 = 0, p2 = 0;
+  for(int i = 1; i < numCubes; i++){
+    p2 = i; while(p1 < p2 && cubes[p1] + tolerance < cubes[p2]) p1++;
+    res = (res*(p2-p1+1))%mod;
+  }
+  std::cout << res;
+  return 0;
+} 
