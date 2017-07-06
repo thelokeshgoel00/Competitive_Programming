4ff9413472b529ac0d300b36de0efc677e4c9a42
vmaddur <vamaddur@gmail.com>
2017-07-06 16:11:05 +0530

    USACO 2012 Silver November Contest (http://www.usaco.org/index.php?page=viewproblem2&cpid=190)

---
+++ USACO/2012 Silver November Contest/clumsy.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int ret = 0, temp = 0;
+string stuff;
+stack<char> brackets;
+
+int main(){
+    freopen("clumsy.in", "r", stdin); freopen("clumsy.out", "w", stdout);
+    cin >> stuff;
+    for(int i = 0; i < stuff.length(); i++){
+        char c = stuff[i];
+        if(c == ')' && brackets.size() > 0 && brackets.top() == '(') brackets.pop();
+        else brackets.push(c);
+    }
+    ret = brackets.size()/2;
+    while(brackets.size() > 0 && brackets.top() == '('){ brackets.pop(); temp++; }
+    ret += temp%2;
+    cout << ret << endl;
+    return 0;
+}
