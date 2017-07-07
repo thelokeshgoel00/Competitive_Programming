9cb0bba8aabfb88ada8598189cb3fe9ea04d21de
vmaddur <vamaddur@gmail.com>
2017-07-07 10:26:25 +0530

    Senpai Arup's Competitive Programming SI

---
+++ Miscellaneous/prankwar.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+    //freopen("shufflegold.in","r",stdin); freopen("shufflegold.out","w",stdout);
+    int n, w, i; scanf("%d %d", &n, &w); int arr [n];
+    for(i = 0; i < n; i++) scanf("%d", &arr[i]);
+    sort(arr, arr+n); int sum = 0;
+    for(i = 0; i < n; i++){
+        sum += arr[i];
+        if(sum > w) break;
+    }
+    cout << i << '\n';
+    return 0;
+}
