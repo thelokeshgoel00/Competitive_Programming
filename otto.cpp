3045140f56fb955dfb895ffe7108dc13825cd847
vmaddur <vamaddur@gmail.com>
2017-07-11 00:23:40 +0530

    Update otto.cpp

--- otto.cpp
+++ otto.cpp
@@ -14,12 +14,12 @@ pair<double, double> cars [100005];
 int main(){
     scanf("%d", &n);
     for(int i = 0; i < n; i++) scanf("%lf %lf", &cars[i].first, &cars[i].second);
+    cars[n] = make_pair(0.0, 0.0);
     curSpeed = cars[0].second;
-    for(int i = 1; i < n; i++){
+    for(int i = 1; i <= n; i++){
         ret += (cars[i-1].first-cars[i].first)/curSpeed;
         curSpeed = max(curSpeed, cars[i].second);
     }
-    ret += cars[n-1].first/curSpeed;
     printf("%.6lf\n", ret);
     return 0;
 }
