e4366084a2d637bef16347d7a7a9c9964194d0d7
vmaddur <vamaddur@gmail.com>
2017-07-11 01:33:21 +0530

    Update otto.cpp

--- otto.cpp
+++ otto.cpp
@@ -3,23 +3,21 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <queue>
 #include <string.h>
 
 using namespace std;
 
-int n;
-double ret = 0.0, curSpeed;
-pair<double, double> cars [100005];
+int numCars;
+double pos [100010], speed [100010], ret = 0.0, fast = 0.0;
 
 int main(){
-    scanf("%d", &n);
-    for(int i = 0; i < n; i++) scanf("%lf %lf", &cars[i].first, &cars[i].second);
-    cars[n] = make_pair(0.0, 0.0);
-    curSpeed = cars[0].second;
-    for(int i = 1; i <= n; i++){
-        ret += (cars[i-1].first-cars[i].first)/curSpeed;
-        curSpeed = max(curSpeed, cars[i].second);
+    cin >> numCars; pos[numCars] = 0; speed[numCars] = -1;
+    for(int i = 0; i < numCars; i++) cin >> pos[i] >> speed[i];
+    for(int i = 0; i < numCars; i++){
+        fast = max(fast, speed[i]);
+        ret += (pos[i]-pos[i+1])/fast;
     }
-    printf("%.6lf\n", ret);
+    printf("%lf\n", ret);
     return 0;
 }
