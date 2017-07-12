79e71e1a7bf631ab1b3cae5d5a042b7101184ab8
vmaddur <vamaddur@gmail.com>
2017-07-12 22:33:26 +0530

    Create excellence.cpp

---
+++ excellence.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <set>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+    int n, mini = 2000001, arr [100000]; scanf("%d", &n);
+    for(int i = 0; i < n; i++) scanf("%d", &arr[i]);
+    for(int i = 0; i < n/2; i++) mini = min(mini, arr[i]+arr[n-1-i]);
+    cout << mini << '\n';
+    return 0;
+}
