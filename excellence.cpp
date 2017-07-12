a9276a21cc3d96577f50b77d6c9e1308925d8a31
vmaddur <vamaddur@gmail.com>
2017-07-12 22:48:53 +0530

    Update excellence.cpp

--- excellence.cpp
+++ excellence.cpp
@@ -12,6 +12,7 @@ using namespace std;
 int main(){
     int n, mini = 2000001, arr [100000]; scanf("%d", &n);
     for(int i = 0; i < n; i++) scanf("%d", &arr[i]);
+    sort(arr, arr+n);
     for(int i = 0; i < n/2; i++) mini = min(mini, arr[i]+arr[n-1-i]);
     cout << mini << '\n';
     return 0;
