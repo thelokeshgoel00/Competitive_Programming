ee4e6cfd1c1af98a18ccd3ce3b1f5ae4f9df6025
vmaddur <vamaddur@gmail.com>
2017-07-19 01:50:35 +0530

    Update mirrors.cpp

--- mirrors.cpp
+++ mirrors.cpp
@@ -13,32 +13,11 @@ using namespace std;
 int n, a, b;
 vector<pair<int, int>> places;
 
-int fullCheck(int diff){
-    if(diff%2 == 1) return 2;
+int checkIt(int diff){
     int ret = 1;
     for(int i = 1; i < a+b; i++){
-        if(places[i].second != places[i-1].second && places[i].first-places[i-1].first%diff == diff/2) continue;
-        if(places[i].second == places[i-1].second && places[i].first-places[i-1].first%diff == 0) continue;
-        ret = 2; break;
-    }
-    return ret;
-}
-
-int halfCheck(int diff){
-    int ret = 1;
-    for(int i = 1; i < a+b; i++){
-        if(places[i].second != places[i-1].second && places[i].first-places[i-1].first%(2*diff) == diff) continue;
-        if(places[i].second == places[i-1].second && places[i].first-places[i-1].first%(2*diff) == 0) continue;
-        ret = 2; break;
-    }
-    return ret;
-}
-
-int oneCheck(){
-    int ret = 1;
-    for(int i = 1; i < a+b; i++){
-        if(places[i].second != places[i-1].second && places[i].first-places[i-1].first%2 == 1) continue;
-        if(places[i].second == places[i-1].second && (places[i].first-places[i-1].first)%2 == 0) continue;
+        if(places[i].second != places[i-1].second && (places[i].first-places[i-1].first)%(2*diff) == diff) continue;
+        if(places[i].second == places[i-1].second && (places[i].first-places[i-1].first)%(2*diff) == 0) continue;
         ret = 2; break;
     }
     return ret;
@@ -51,7 +30,12 @@ int main(){
     for(int i = 0; i < b; i++){ int x; cin >> x; places.push_back(make_pair(x, 1)); }
     sort(places.begin(), places.end());
     if(a+b < 2){ cout << a+b << "\n"; return 0; }
-    int diff = places[0].first;
-    cout << min(oneCheck(), min(fullCheck(diff), halfCheck(diff))) << '\n';
+    int maxDiff = places[0].first; int display = 2;
+    for(int i = 1; i*i <= maxDiff; i++){
+        if(maxDiff%i != 0) continue;
+        display = min(display, checkIt(maxDiff/i));
+        display = min(display, checkIt(i));
+    }
+    cout << display << '\n';
     return 0;
 }
