28500dbfc79aa92a1a89986233b74b7a9400c073
vmaddur <vamaddur@gmail.com>
2017-07-20 01:39:11 +0530

    Update robot.cpp

--- robot.cpp
+++ robot.cpp
@@ -12,22 +12,22 @@ using namespace std;
 
 int N;
 double dp [1010], penalties [1010];
-pair<double, double> checkpoints [1010];
+pair<double, double> checkpts [1010];
 
 double dist(int a, int b){
-    return sqrt(pow(checkpoints[a].first-checkpoints[b].first, 2)+pow(checkpoints[a].second-checkpoints[b].second, 2));
+    return sqrt(pow(checkpts[a].first-checkpts[b].first, 2)+pow(checkpts[a].second-checkpts[b].second, 2));
 }
 
 int main(){
-    checkpoints[1] = make_pair(0.0, 0.0); penalties[1] = 0.0; dp[1] = 0.0;
+    checkpts[1] = make_pair(0.0, 0.0); penalties[1] = 0.0; dp[1] = 0.0;
     while(true){
         cin >> N;
         if(N == 0) break;
         for(int i = 2; i <= N+1; i++){
-            cin >> checkpoints[i].first >> checkpoints[i].second >> penalties[i];
+            cin >> checkpts[i].first >> checkpts[i].second >> penalties[i];
             penalties[i] += penalties[i-1];
         }
-        checkpoints[N+2] = make_pair(100.0, 100.0); penalties[N+2] = penalties[N-1];
+        checkpts[N+2] = make_pair(100.0, 100.0); penalties[N+2] = penalties[N-1];
         for(int i = 2; i <= N+2; i++){
             dp[i] = 10000000.0;
             for(int last = 1; last < i; last++) dp[i] = min(dp[i], dp[last]+1+dist(i, last)+penalties[i-1]-penalties[last]);
