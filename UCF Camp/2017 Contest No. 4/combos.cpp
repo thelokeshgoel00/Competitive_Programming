2ea5cf9c3a45a8c6d2af315f15d9dfb1751feda8
vmaddur <vamaddur@gmail.com>
2017-07-19 22:36:23 +0530

    Generating the Nth Lexicographically Smallest Subset (http://www.keit…  …hschwarz.com/binary-subsets/)

--- UCF Camp/2017 Contest No. 4/combos.cpp
+++ UCF Camp/2017 Contest No. 4/combos.cpp
@@ -36,3 +36,34 @@ int main(){
     cout << '\n';
     return 0;
 }
+
+/*
+Alternate Method
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+const int L = 60, N = 1e5;
+typedef long long ll;
+ 
+ll f[L];
+char s[N];
+int main(){
+    f[0] = 1;
+    for (int i = 1; i < L; ++ i)
+        f[i] = f[i - 1] * 2LL;
+    int n; cin >> n;
+    for (int i = 0; i < n; ++ i)
+        cin >> s[i];
+    ll k; cin >> k;
+    vector <int> ans;
+    int cur_l = 0;
+    while (k){
+        while (k > f[n - cur_l - 1])
+            k -= f[n - cur_l - 1], ++ cur_l;
+        ans.push_back(cur_l), ++ cur_l, -- k;
+    }
+    for (auto i : ans) cout << s[i] << " ";
+    return 0;
+}
+*/
