ef6b973a0e74eff5f8f9bf7e53e57fa03eafef26
vmaddur <vamaddur@gmail.com>
2017-07-21 02:47:32 +0530

    Update board.cpp

--- board.cpp
+++ board.cpp
@@ -12,37 +12,55 @@
 
 using namespace std;
 
-int n, ret = 15;
-set<int> positions;
-
-int checkMask(int mask){
-    vector<pair<int, int>> startPos; int numBits = 0;
-    set<int> covered;
-    for(int i = 0; i < 8; i++) startPos.push_back(make_pair(0, i));
-    for(int i = 1; i < 8; i++) startPos.push_back(make_pair(i, 0));
-    for(int i = 0; i < 15; i++){
-        if((mask&(1<<i)) == 0) continue;
-        numBits++;
-        int x = startPos[i].first, y = startPos[i].second, dx = 1, dy = 1;
-        while(x > -1 && y > -1 && x < 8 && y < 8){
-            int temp = x*100+y;
-            if(positions.find(temp) != positions.end()) covered.insert(temp);
-            x += dx; y += dy;
-        }
-    }
-    if(covered.size() == positions.size()) return numBits;
-    return -1;
-}
+int n, ret;
+pair<int, int> stuff [80];
+map<int, int> hashToIndex;
+bool used [80];
 
 int main(){
-    cin >> n;
+    cin >> n; ret = n; memset(used, false, sizeof(used));
     for(int i = 0; i < n; i++){
-        int a, b; cin >> a >> b;
-        positions.insert(a*100+b-101);
+        cin >> stuff[i].first >> stuff[i].second;
+        stuff[i].first--; stuff[i].second--;
+        hashToIndex[stuff[i].first*100+stuff[i].second] = i;
     }
-    for(int i = 0; i < (1<<15); i++){
-        int gg = checkMask(i);
-        if(gg != -1) ret = min(ret, gg);
+    for(int i = 0; i < n; i++){
+        if(used[i]) continue;
+        used[i] = true;
+        int rightd = 0, leftd = 0, x = stuff[i].first+1, y = stuff[i].second+1, dx = 1, dy = 1;
+        while(x < 8 && y < 8){
+            if(hashToIndex.find(100*x+y) != hashToIndex.end() && !used[hashToIndex.find(100*x+y)->second]){
+                rightd++;
+                used[hashToIndex.find(100*x+y)->second] = true;
+            }
+            x += dx; y += dy;
+        }
+        x = stuff[i].first-1, y = stuff[i].second-1, dx = -1, dy = -1;
+        while(x > -1 && y > -1){
+            if(hashToIndex.find(100*x+y) != hashToIndex.end() && !used[hashToIndex.find(100*x+y)->second]){
+                rightd++;
+                used[hashToIndex.find(100*x+y)->second] = true;
+            }
+            x += dx; y += dy;
+        }
+        if(rightd > 0){ ret -= rightd; continue; }
+        x = stuff[i].first+1, y = stuff[i].second-1, dx = 1, dy = -1;
+        while(x < 8 && y > -1){
+            if(hashToIndex.find(100*x+y) != hashToIndex.end() && !used[hashToIndex.find(100*x+y)->second]){
+                leftd++;
+                used[hashToIndex.find(100*x+y)->second] = true;
+            }
+            x += dx; y += dy;
+        }
+        x = stuff[i].first-1, y = stuff[i].second+1, dx = -1, dy = 1;
+        while(x > -1 && y < 8){
+            if(hashToIndex.find(100*x+y) != hashToIndex.end() && !used[hashToIndex.find(100*x+y)->second]){
+                leftd++;
+                used[hashToIndex.find(100*x+y)->second] = true;
+            }
+            x += dx; y += dy;
+        }
+        ret -= leftd;
     }
     cout << ret << '\n';
     return 0;
