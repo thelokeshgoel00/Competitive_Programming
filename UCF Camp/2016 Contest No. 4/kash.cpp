695a1e37e208e87c9bf971802eff18b9b282f0a3
vmaddur <vamaddur@gmail.com>
2017-07-28 00:54:05 +0530

    Basic Dynamic Programming

--- UCF Camp/2016 Contest No. 4/kash.cpp
+++ UCF Camp/2016 Contest No. 4/kash.cpp
@@ -13,16 +13,18 @@
 
 using namespace std;
 
-int d, vals [101], costs [101], dp [101][101], ret = 0;
+int d, dp [101][101], ret = 0;
 
 int main(){
     cin >> d; memset(dp, 0, sizeof(dp));
-    for(int i = 1; i <= d; i++) cin >> costs[i] >> vals[i];
-    for(int i = 1; i <= d; i++)
-        for(int j = costs[i]; j <= i; j++){
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-costs[i]]+vals[i]);
+    for(int i = 1; i <= d; i++){
+        int cost, val; cin >> cost >> val;
+        for(int j = 0; j <= i; j++){
+            dp[i][j] = dp[i-1][j];
+            if(j >= cost) dp[i][j] = max(dp[i][j], dp[i-1][j-cost]+val);
             ret = max(ret, dp[i][j]);
         }
+    }
     cout << ret << '\n';
     return 0;
 }
