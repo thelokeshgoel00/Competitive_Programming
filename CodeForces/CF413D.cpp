1d18e7bd31cd4749c7ad0108043240fc7f0b7deb
Viraj Maddur <vamaddur@gmail.com>
2017-10-15 16:05:56 -0400

    Game Dynamic Programming (http://codeforces.com/contest/413/problem/D)

--- CodeForces/CF413D.cpp
+++ CodeForces/CF413D.cpp
@@ -1,18 +1,25 @@
 #define __USE_MINGW_ANSI_STDIO 0
 #include <iostream>
+#include <iomanip>
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
 #include <algorithm>
 #include <queue>
 #include <map>
+#include <unordered_map>
 #include <set>
+#include <unordered_set>
 #include <stack>
+#include <deque>
 #include <string.h>
 #include <math.h>
 
 using namespace std;
 
+#define PI 4.0*atan(1.0)
+#define epsilon 0.000000001
+#define INF 1000000000000000000
 #define MOD 1000000007
 
 int n, k, arr [2010], pow2 [] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192};
