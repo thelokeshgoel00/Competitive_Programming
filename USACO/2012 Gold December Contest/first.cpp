8ae63a1c0a940f5429220751198174f1004f48eb
Viraj Maddur <vamaddur@gmail.com>
2017-12-01 02:23:22 -0500

    Prefix Trie

--- USACO/2012 Gold December Contest/first.cpp
+++ USACO/2012 Gold December Contest/first.cpp
@@ -1,3 +1,91 @@
+#define __USE_MINGW_ANSI_STDIO 0
+#include <iostream>
+#include <iomanip>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <stack>
+#include <deque>
+#include <string.h>
+#include <cstring>
+#include <sstream>
+#include <math.h>
+
+using namespace std;
+
+#define PI 4.0*atan(1.0)
+#define epsilon 0.000000001
+#define INF 1000000000000000000
+#define MOD 1000000007
+
+int N, idx = 1, trie [300010][26], inDegree [26];
+string arr [30010];
+bool isEnd [300010], adjacency [26][26];
+vector<string> ret;
+
+int main(){
+    freopen("first.in", "r", stdin); freopen("first.out", "w", stdout);
+    ios_base::sync_with_stdio(0); cin.tie(0); cout << fixed << setprecision(18);
+    cin >> N; memset(trie, -1, sizeof(trie)); memset(isEnd, false, sizeof(isEnd));
+    for(int i = 0; i < N; i++){
+        cin >> arr[i];
+        int pos = 0;
+        for(int j = 0; j < arr[i].length(); j++){
+            if(trie[pos][arr[i][j]-'a'] == -1) trie[pos][arr[i][j]-'a'] = idx++;
+            pos = trie[pos][arr[i][j]-'a'];
+        }
+        isEnd[pos] = true;
+    }
+    for(int i = 0; i < N; i++){
+        memset(adjacency, false, sizeof(adjacency)); memset(inDegree, 0, sizeof(inDegree));
+        int pos = 0; bool endFound = false;
+        for(int j = 0; j < arr[i].length(); j++){
+            if(isEnd[pos]){ endFound = true; break; }
+            for(int k = 0; k < 26; k++)
+                if(trie[pos][k] != -1 && arr[i][j]-'a' != k)
+                    adjacency[arr[i][j]-'a'][k]++;
+            pos = trie[pos][arr[i][j]-'a'];
+        }
+        if(endFound) continue; // Another string is the prefix of this string, so this string can never be lexicographically first
+        for(int a = 0; a < 26; a++)
+            for(int b = 0; b < 26; b++)
+                if(adjacency[a][b])
+                    inDegree[b]++;
+        int numZero = 0; queue<int> q;
+        for(int a = 0; a < 26; a++)
+            if(inDegree[a] == 0)
+                q.push(a);
+        while(q.size() > 0){
+            int curr = q.front(); q.pop(); numZero++;
+            for(int a = 0; a < 26; a++)
+                if(adjacency[curr][a] && inDegree[a] > 0){
+                    inDegree[a]--;
+                    if(inDegree[a] == 0) q.push(a);
+                }
+        }
+        if(numZero < 26) continue; // The potential ordering of the alphabet contains a cycle
+        /*bool hasCycle = false;
+        for(int c = 0; c < 26; c++)
+            for(int a = 0; a < 26; a++)
+                for(int b = 0; b < 26; b++){
+                    adjacency[a][b] |= adjacency[a][c] && adjacency[c][b];
+                    if(a == b && adjacency[a][b]) hasCycle = true;
+                }
+        if(hasCycle) continue;*/
+        ret.push_back(arr[i]);
+    }
+    cout << ret.size() << '\n';
+    for(string s : ret) cout << s << '\n';
+    return 0;
+}
+
+/*
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -72,3 +160,4 @@ int main(){
     for(int i = 0; i < ret.size(); i++) cout << ret[i] << '\n';
     return 0;
 }
+*/
