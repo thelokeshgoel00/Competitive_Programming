3adca7515d3829e9dbf7fadb5bf0ba79d7e69394
Viraj Maddur <vamaddur@gmail.com>
2018-03-10 17:48:45 -0500

    Game Bracket Dynamic Programming (http://codeforces.com/contest/859/problem/D)

--- CodeForces/CF859D.cpp
+++ CodeForces/CF859D.cpp
@@ -1,21 +1,26 @@
 #define __USE_MINGW_ANSI_STDIO 0
 #include <iostream>
+#include <iomanip>
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
 #include <algorithm>
 #include <queue>
 #include <map>
+#include <unordered_map>
 #include <set>
+#include <unordered_set>
 #include <stack>
+#include <deque>
 #include <string.h>
+#include <sstream>
 #include <math.h>
 
 using namespace std;
 
-#define PI 3.141592653589793
+#define PI atan2(0, -1)
 #define epsilon 0.000000001
-#define INF 1000000000000000000
+#define INF 5000000000000000000
 #define MOD 1000000007
 
 int N, K;
