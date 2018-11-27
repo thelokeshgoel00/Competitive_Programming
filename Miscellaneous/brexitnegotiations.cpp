9e28180fefeb16dd7116830680f9bc9b6fb342d1
Viraj Maddur <vamaddur@gmail.com>
2018-11-26 22:13:18 -0500

    Greedy Ad Hoc (https://open.kattis.com/problems/brexitnegotiations)

---
+++ Miscellaneous/brexitnegotiations.cpp
@@ -0,0 +1,74 @@
+#define __USE_MINGW_ANSI_STDIO 0
+#include <iostream>
+#include <iomanip>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <stack>
+#include <deque>
+#include <string.h>
+#include <sstream>
+#include <bitset>
+#include <math.h>
+#include <assert.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+template <class T>
+using ordered_set = __gnu_pbds::tree<T, __gnu_pbds::null_type, less<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
+template <class T>
+using ordered_multiset = __gnu_pbds::tree<T, __gnu_pbds::null_type, less_equal<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
+
+#define PI atan2(0, -1)
+#define epsilon 0.000000001
+#define INF 5000000000000000000
+#define MOD 1000000007
+
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+
+int N, lengths [400010], degree [400010], ret = 0;
+vector<int> dependencies [400010];
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+int main(){
+    //freopen("sort.in", "r", stdin); freopen("sort.out", "w", stdout);
+    ios_base::sync_with_stdio(0); cin.tie(0); cout << fixed << setprecision(10);
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        int d; cin >> lengths[i] >> d;
+        for(int j = 0; j < d; j++){
+            int x; cin >> x; x--;
+            dependencies[i].pb(x); degree[x]++;
+        }
+    }
+    for(int i = 0; i < N; i++)
+        if(degree[i] == 0)
+            pq.push(mp(lengths[i], i));
+    for(int i = N-1; i > -1; i--){
+        pair<int, int> now = pq.top(); pq.pop();
+        ret = max(ret, now.f+i);
+        for(int nexty : dependencies[now.s])
+            if(--degree[nexty] == 0)
+                pq.push(mp(lengths[nexty], nexty));
+    }
+    cout << ret << '\n';
+    return 0;
+}
+
+/******************************
+Kateba ii dake no hanashi darou
+Success is only a victory away
+- No Game No Life Opening
+******************************/
