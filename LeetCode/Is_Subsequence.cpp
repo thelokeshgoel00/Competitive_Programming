ac589abd0d5a12c3c4b5ffa6063c18459e0999d4
harshit977 <66196066+harshit977@users.noreply.github.com>
2020-10-03 18:21:37 +0530

    created same using java

--- LeetCode/Is_Subsequence.cpp
+++ LeetCode/Is_Subsequence.cpp
@@ -1,21 +1,35 @@
-class Solution {
-public:
-    bool isSubsequence(string s, string t) {
-        int i,j=0;                  //keeping j=0 initially
-        if(t.length()<s.length())
-            return false;
-        for(i=0;i<t.length();i++)
-        {
-            if(t[i]==s[j])
-            {
-                j++;                //incrementing j by 1, so as to compare next character
-                if(j==s.length())
-                    break;
-            }
-        }
-        if(j==s.length())
-            return true;
-        else 
-            return false;
-    }
-};
+package java_assignment;
+
+import java.util.Scanner;
+
+public class ques_7 
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter First String: ");
+		String str1=sc.nextLine();
+		System.out.print("Enter Second String: ");
+		String str2=sc.nextLine();
+		int count=0;
+		for(int i=0;i<(str2.length()-str1.length())+1;i++)
+		{
+			String substr=str2.substring(i,str1.length()+i);
+			if(str1.compareTo(substr)==0)
+			{
+				count++;
+			}
+			substr="";
+		}
+		if(count>0)
+		{
+			System.out.println("String 2 contains string 1 ");
+			System.out.print("No of occurences: "+count);
+		}
+		else
+		{
+			System.out.print("String 2 does not contain string 1");
+		}
+		sc.close();
+	}
+}
