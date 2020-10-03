c9c6e672a600135095af3d5e07678a2d903e2d0d
Sahil <72294816+sas-28@users.noreply.github.com>
2020-10-03 14:34:03 +0530

    A. FashionabLee (https://codeforces.com/contest/1369/problem/A)

---
+++ 1369A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve()
+{
+	long long n;
+	cin>>n;
+	if(n%4==0)
+		cout<<"YES"<<endl;
+	else
+		cout<<"NO"<<endl;
+}
+int main()
+{
+	long t;	
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	return 0;
+}
