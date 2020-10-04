5231574df397b854eb6749f1c4501ea25768601b
riya-arora08 <72336481+riya-arora08@users.noreply.github.com>
2020-10-04 17:18:11 +0530

    ArrayReversal

--- Hackerrank/ArrayReversal.c
+++ Hackerrank/ArrayReversal.c
@@ -6,6 +6,30 @@
 
 int main()
 {
+    int num, *arr,*rev_arr, i,j;
+    printf("Enter the number of elements ");
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    rev_arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+
+    for(i = num-1,j=0; i>=0&&j<num; i--,j++){
+      *(rev_arr+j)=*(arr+i);
+    }
+    for(int i=0;i<num;i++)
+    {
+        *(arr+i)=*(rev_arr+i);
+        printf("%d ",*(arr+i));
+    }
+    return 0;
+}
+
+
+/*
+int main()
+{
     int num, *arr, i, temp;
     scanf("%d", &num);
     arr = (int *)malloc(num * sizeof(int));
