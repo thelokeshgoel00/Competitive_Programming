1f79bf157296d8d4e2134fdd83523746c179e80c
Ansuman <sonuansuman833@gmail.com>
2020-10-12 22:15:48 +0530

    Adding lovelyno. program solution- Hacktoberfest

---
+++ HackerEarth/lovelyno.java
@@ -0,0 +1,29 @@
+ 
+
+import java.util.*;
+
+public class LovelyNo {
+
+	public static void main(String[] args) {
+		int cd=0;
+	 
+		Scanner s= new Scanner(System.in);
+		int T=s.nextInt();
+		while(T-- >0) {
+			int a=s.nextInt();
+			int b=s.nextInt();
+			for(int j=1;j<=a && j<=b;j++) {
+				 if(a % j==0 && b % j==0)
+		              cd = j;
+				  
+		}
+	  
+		System.out.println(cd );
+		 cd=1;
+		}
+		
+	}
+
+}
+ 
+ 
